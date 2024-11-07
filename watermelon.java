dc7aaf7eb9ee31e95a032c6275be2a4e1c41f8ad
Mohamed Saad <111496655+hamo229@users.noreply.github.com>
2024-11-07 11:55:52 -0800

    Create watermelon.java

---
+++ watermelon.java
@@ -0,0 +1,19 @@
+package moha;
+
+import java.util.Scanner;
+
+public class watermelon {
+
+	public static void main(String[] args) {
+Scanner input= new Scanner(System.in);
+int namber;
+System.out.println("enter weight of the watermelon ");
+namber= input.nextInt();
+if (namber%2==0){
+    System.out.println("YES");
+}else {
+    	System.out.println("NO");
+    }
+	}
+
+}
